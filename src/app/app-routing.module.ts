c62fe98e098a1851dd24ef304fdce1fd0a3bf8b9
AKASH SANTOSH CHAUDHARI <65148480+akash2941@users.noreply.github.com>
2020-09-05 02:01:49 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,35 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordComponent } from './password/password.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CategaryComponent } from './categary/categary.component';
+import { FooterComponent } from './footer/footer.component';
+import { PaymentComponent } from './payment/payment.component';
+import { LogfooterComponent } from './logfooter/logfooter.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { CartComponent } from './cart/cart.component';
+
 
 const routes: Routes = [
-  { path: "forget",component: ForgetComponent },
+ 
   { path: "signup",component: SignupComponent },
   { path: "login",component: LoginComponent },
   { path: "password",component: PasswordComponent },
-  { path: "header",component: HeaderComponent }
+  { path: "header",component: HeaderComponent },
+  { path: "home",component: HomeComponent },
+  { path: "categary",component: CategaryComponent },
+  { path: "footer",component: FooterComponent},
+  { path: "payment",component: PaymentComponent},
+  { path: "logfooter",component: LogfooterComponent},
+  { path: "contactus",component: ContactusComponent},
+  { path: "cart",component: CartComponent},
+  
+  
  
-
+  
+ 
+  { path: "", redirectTo:'/home', pathMatch:'full'},
+  { path: "**",component: ForgetComponent }
 
 ];
 
